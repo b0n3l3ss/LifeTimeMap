18e0e5d0550e01a8e3b6cb2ddc7e66b494cc0003
Jonas F. <jonasiboy.11@gmail.com>
2024-10-17 12:35:16 -0700

    Adding html and js files

---
+++ script.js
@@ -0,0 +1,11 @@
+let d = new Date();
+document.body.innerHTML = "<p>Today's date is " + d + "</p>"
+
+var shown = 'options';
+function show(college) {
+	var form = document.getElementById('searchform');
+	var college = form.elements.searchfield.value;
+	document.getElementById(shown).style.display='none';
+	document.getElementById(college).style.display='block';
+	shown = college;
+}
