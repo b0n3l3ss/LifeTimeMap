983b0e953c9eac6cecba38d603cba0c997f423ba
Jonas F. <jonasiboy.11@gmail.com>
2024-10-18 08:27:47 -0700

    Making HTML and JS work better together using scripts

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let d = new Date();
 document.body.innerHTML = "<p>Today's date is " + d + "</p>"
 
-var shown = 'options';
-function show(college) {
-	var form = document.getElementById('searchform');
-	var college = form.elements.searchfield.value;
-	document.getElementById(shown).style.display='none';
-	document.getElementById(college).style.display='block';
-	shown = college;
+function show() {
+	let d = new Date();
+	document.body.innerHTML = "<p>Today's date is " + d + "</p>"
 }
+var shown = 'options';
+//function show(college) {
+//	var form = document.getElementById('searchform');
+//	var college = form.elements.searchfield.value;
+//	document.getElementById(shown).style.display='none';
+//	document.getElementById(college).style.display='block';
+//	shown = college;
+//	document.body.innerHTML = "<p>Today's date is " + d + "</p>"
+//}
