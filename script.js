e976b8ff3360e3c0d49725b2df98efe9a794eacb
Jonas F. <jonasiboy.11@gmail.com>
2024-10-18 08:31:32 -0700

    Refinements to JS script

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let d = new Date();
-document.body.innerHTML = "<p>Today's date is " + d + "</p>"
-
-function show() {
-	let d = new Date();
-	document.body.innerHTML = "<p>Today's date is " + d + "</p>"
+const btn1 = document.getElementById('btn1');
+const txt1 = document.getElementById('tbuser');
+const out1 = document.getElementById('output1');
+function fun1(){
+	out1.innerHTML = txt1.value
 }
-var shown = 'options';
-//function show(college) {
-//	var form = document.getElementById('searchform');
-//	var college = form.elements.searchfield.value;
-//	document.getElementById(shown).style.display='none';
-//	document.getElementById(college).style.display='block';
-//	shown = college;
+btn1.addEventListener('click', fun1);
+
+
+//let d = new Date();
+//document.body.innerHTML = "<p>Today's date is " + d + "</p>"
+
+//function show() {
+//	let d = new Date();
 //	document.body.innerHTML = "<p>Today's date is " + d + "</p>"
 //}
+
