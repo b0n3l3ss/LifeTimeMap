1b7731eb6b033754d6383ab96caeb57587472895
Jonas Fahrney <jonas.fahrney@gmail.com>
2024-10-21 09:18:52 -0700

    testing new login

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+//
+
+const ageRegex = /\d{4}/
+
+
 const btn1 = document.getElementById('btn1');
 const txt1 = document.getElementById('tbuser');
 const out1 = document.getElementById('output1');
+const output = document.querySelector("#output")
 function fun1(){
 	out1.innerHTML = txt1.value
 }
+
+function foo(checkedInput){
+	const ok = ageRegex.exec(checkedInput.value)
+
+	output.textContent = ok
+		? 'This is a valid birth year ${ok[0]}'
+		: 'The birth year entered is not valid!'
+}
+
 btn1.addEventListener('click', fun1);
+bt1.addEventListener('click', foo)
 
 
 //let d = new Date();
